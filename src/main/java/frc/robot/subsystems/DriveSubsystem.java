3dc9d08947600b4c4a577e27a1eec07524ef5cd0
namanpradhan1034 <s2gnaman@gmail.com>
2026-06-22 22:04:42 -0700

    first commit! just created some basic constants and will work more tmrw when i get specifics

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    public DriveSubsystem() {
+        // motors will go here later
+    }
+
+    @Override
+    public void periodic() {
+        // runs every 20ms
+    }
+}
